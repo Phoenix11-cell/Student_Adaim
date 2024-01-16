2e909ac2b0c2502605198e29492ba26467bd8068
ZiSenWang <1192567943@qq.com>
2024-01-16 20:47:37 +0800

    0118commit

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,95 +1,49 @@
 <script setup>
-import { reactive, onMounted } from "vue";
+import { reactive, onMounted, computed } from "vue";
 
 const state = reactive({
   students: [],
-  images: [],
+  itemPerPage: 20,
+  currentPage: 1,
+});
+
+const currentPageItems = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage.value;
+  const endIndex = startIndex + itemsPerPage.value;
+  return state.students.value.slice(startIndex, endIndex);
 });
 
 onMounted(async () => {
   const student_rsp = await fetch(
-    "https://fakerapi.it/api/v1/persons?_quantity=20"
+    "https://fakerapi.it/api/v1/persons?_quantity=10"
   );
-  state.students = await student_rsp.json();
+  const students_json = await student_rsp.json();
+  const students = students_json.data;
 
-  const image_rsp = await fetch(
-    "https://fakerapi.it/api/v1/images?_quantity=20&_type=kittens&_height=300"
-  );
-  state.images = await image_rsp.json();
+  state.students = students.map((item) => {
+    return { ...item, image: "https://placekitten.com/160/140" };
+  });
 });
-
-// onMounted(async () => {
-//   const data = await fetch("https://fakerapi.it/api/v1/persons?_quantity=20")
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((json) => {
-//       return json.data;
-//     });
-
-//   const images = await fetch(
-//     "https://fakerapi.it/api/v1/images?_quantity=20&_type=kittens&_height=300"
-//   )
-//     .then((rsp) => {
-//       console.log(rsp.json());
-//       return rsp.json();
-//     })
-//     .then((json) => {
-//       return json.url;
-//     });
-//   console.log(images);
-//   state.students = data;
-// });
 </script>
 
 <template>
   <div class="students">
-    <div class="student">
+    <div class="student" v-for="(student, idx) in state.students" :key="idx">
       <div
         class="student-image"
-        style="
-          background-image: url('https://fastly.picsum.photos/id/274/200/300.jpg?hmac=1D3k-fgapICO1fYT7KMrtIquVz8pPybJ4ByxPwwXWRg');
-        "
+        :style="{ backgroundImage: 'url(' + student.image + ')' }"
       ></div>
-      <h4>name:111</h4>
-      <h4>city:111</h4>
-      <h4>phone:111</h4>
+      <h4>{{ student.firstname + " " + student.lastname }}</h4>
+      <h4>{{ student.email }}</h4>
+      <h4>{{ student.phone }}</h4>
       <div class="button-container">
         <button class="details">Details</button>
         <button class="delete">Delete</button>
       </div>
     </div>
-
-    <div class="student">
-      <div
-        class="student-image"
-        style="
-          background-image: url('https://fastly.picsum.photos/id/274/200/300.jpg?hmac=1D3k-fgapICO1fYT7KMrtIquVz8pPybJ4ByxPwwXWRg');
-        "
-      ></div>
-      <h4>name:222</h4>
-      <h4>city:222</h4>
-      <h4>phone:222</h4>
-      <div class="button-container">
-        <button class="details">Details</button>
-        <button class="delete">Delete</button>
-      </div>
-    </div>
-
-    <div class="student">
-      <div
-        class="student-image"
-        style="
-          background-image: url('https://fastly.picsum.photos/id/274/200/300.jpg?hmac=1D3k-fgapICO1fYT7KMrtIquVz8pPybJ4ByxPwwXWRg');
-        "
-      ></div>
-      <h4>name:333</h4>
-      <h4>city:333</h4>
-      <h4>phone:333</h4>
-      <div class="button-container">
-        <button class="details">Details</button>
-        <button class="delete">Delete</button>
-      </div>
+    <div class="buttom-buttons">
+      <button>上一页</button>
+      <button>下一页</button>
     </div>
   </div>
 </template>
@@ -104,9 +58,12 @@ onMounted(async () => {
     flex: 0 0 20%;
     box-sizing: border-box;
     box-shadow: 0 4px 8px 0 rgba(201, 13, 13, 0.4);
-    padding: 40px;
+    padding: 10px 60px;
     margin: 10px;
     height: 400px;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
 
     @media only screen and (max-width: 769px) {
       flex: 0 0 40%;
@@ -126,9 +83,12 @@ onMounted(async () => {
     }
     h4 {
       margin: 22px auto;
-      font-size: 16px;
+      font-size: 15px;
       max-width: 60%;
       font-weight: normal;
+      white-space: nowrap;
+      box-sizing: border-box;
+      width: 100%;
     }
 
     .button-container {
@@ -158,76 +118,27 @@ onMounted(async () => {
       }
     }
   }
-}
-</style>
 
-<style lang="scss">
-.home {
-  .products {
+  .buttom-buttons {
+    width: 100%; /* 让按钮占满整个宽度 */
+    margin-top: auto; /* 将按钮推到底部 */
     display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    justify-content: center; /* 水平居中按钮 */
+    margin-bottom: 20px; /* 添加底部间距，根据需要调整 */
 
-    .product {
-      flex: 0 0 30%;
-      box-sizing: border-box;
-      box-shadow: 0 4px 8px 0 rgba(201, 13, 13, 0.2);
-      padding: 16px;
-      margin: 8px;
-      height: 360px;
+    button {
+      color: #fff;
+      background-color: #007bff;
+      border: 1px solid #007bff;
+      border-radius: 100px;
+      font-weight: 400;
+      text-align: center;
+      padding: 8px 16px;
+      cursor: pointer;
+      margin-right: 10px;
 
-      @media only screen and (max-width: 769px) {
-        flex: 0 0 40%;
-      }
-
-      @media only screen and (max-width: 640px) {
-        flex: 0 0 90%;
-      }
-
-      &.inBag {
-        border: 1px solid #007bff;
-      }
-
-      .product-image {
-        margin: 20px auto;
-        width: 160px;
-        height: 140px;
-        background-size: contain;
-        background-position: center;
-        background-repeat: no-repeat;
-      }
-      h4 {
-        margin: 22px auto;
-        font-size: 12px;
-        max-width: 60%;
-        font-weight: normal;
-      }
-
-      p.price {
-        font-size: 20px;
-        font-weight: bold;
-      }
-
-      button {
-        color: #fff;
-        background-color: #007bff;
-        border: 1px solid #007bff;
-        border-radius: 100px;
-        font-weight: 400;
-        text-align: center;
-        padding: 8px 16px;
-        cursor: pointer;
-
-        &:hover {
-          opacity: 0.8;
-        }
-
-        &.remove {
-          background-color: transparent;
-          border: none;
-          color: red;
-          text-decoration: underline;
-        }
+      &:hover {
+        opacity: 0.8;
       }
     }
   }
